be6c729e555e1bc5867d8101108f43dc539d0805
Tyler Page <iamtpage@users.noreply.github.com>
2017-03-29 21:18:08 -0400

    Update git-update.sh

--- shell_scripts/git-update.sh
+++ shell_scripts/git-update.sh
@@ -1,14 +1,10 @@
 #! /bin/bash
 
-# Run script validation
+# If it config validation fails, stop script
+echo "Validating configuration..."
 source /srv/homeassistant/homeassistant_venv/bin/activate
-hass --script check_config
 
-# If it fails, stop script
-if [ $? != 0 ]; then
-   echo "Error, config validation failed"
-   exit
-fi
+if hass --script check_config; then
 
 cd /home/homeassistant/.homeassistant
 git add .
@@ -17,5 +13,12 @@ echo -n "Enter the Description for the Change: " [Minor Update]
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
+
+else
+
+echo "Script validation failed"
+exit 1
+
+fi
 
 exit
